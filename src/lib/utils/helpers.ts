3c19bd60db1629d566433a34eaae7c5881d190ef
UnholyXD <unholyxd@gmail.com>
2026-09-04 17:23:13 -0300

    feat: remove obsolete archives

--- src/lib/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const isNonEmptyArray = <T>(value: T[] | undefined): value is T[] =>
-  Array.isArray(value) && value.length > 0;
-
-export const createSlug = (value: string): string =>
-  value
-    .trim()
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-+|-+$/g, '');
